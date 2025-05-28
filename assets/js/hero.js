868e16034c762b996d0d463bf65c614e425fc344
Vaishnavi226 <vaishnavitripathi226@gmail.com>
2025-05-28 10:20:31 +0530

    Updated code

---
+++ assets/js/hero.js
@@ -0,0 +1,28 @@
+const floatingImg = document.querySelectorAll('.floatingImg');
+
+floatingImg.forEach((img, index)=>{
+
+    // change on hovering 
+    img.addEventListener("mouseover",()=>{
+        // 1. main text will go behind
+        //2. image text should be visible 
+    });
+
+    // seeting back chnages on removeing mouse from hover state
+    img.addEventListener("mouseleave",()=>{
+
+    });
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
